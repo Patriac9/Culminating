4224aff27f8a2b28b7f1232b5344d847028587f8
franzha908 <zagymchem@gmail.com>
2024-06-12 12:03:40 -0400

    modify the main frame

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
-
+import Elements.*;
 public class Main {
 
     static final String TITLE="Periodic table";
     public static void main(String[] args) {
         JFrame MainFrame=new JFrame(TITLE);
         MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        MainFrame.setSize(800,450);
+        MainFrame.setSize(1080,720);
         MainFrame.setVisible(true);
+
+        ImageIcon iconimage;
     }
 }
